95f2c362c4abd6dc20c5dd34566cd6ccddba6923
Darunporn <darunporn@da.co.th>
2024-07-11 23:07:12 +0700

    [init]no mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
